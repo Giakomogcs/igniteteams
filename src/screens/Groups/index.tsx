1fb4f3992a0e2412bdf7a687698abf3bb0173314
Giovani <giakomogcs@gmail.com>
2024-04-15 06:50:35 -0300

    render list of GroupCard and empty list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
+
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Aulas Rocketseat",
+    "Amigos",
+  ]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
 
-      <GroupCard title="Galera do IST" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </Container>
   );
 }
